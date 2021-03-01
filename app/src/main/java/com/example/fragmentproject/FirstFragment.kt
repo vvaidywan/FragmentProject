c15e9176ce38178c5abed5cacb666ef0fc22a2da
Varsha <varsha.vaidywan@tothenew.com>
2021-03-02 00:23:56 +0530

    fourth question done

--- app/src/main/java/com/example/fragmentproject/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentproject/FirstFragment.kt
@@ -1,7 +1,31 @@
 package com.example.fragmentproject
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_first, container, false)
+        return view
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val openFragmentbtn = view!!.findViewById<Button>(R.id.openFragmentbtn)
+        openFragmentbtn.setOnClickListener(View.OnClickListener {
+            val fragmentManager = fragmentManager
+            val fragmentTransaction = fragmentManager!!.beginTransaction()
+            fragmentTransaction.add(R.id.frameLayout, SecondFragment(), "Second fragment")
+            fragmentTransaction.commit()
+        })
+    }
 }
--- app/src/main/java/com/example/fragmentproject/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentproject/SecondFragment.kt
@@ -1,7 +1,20 @@
 package com.example.fragmentproject
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_second, container, false)
+        return view
+    }
+
 
 }
